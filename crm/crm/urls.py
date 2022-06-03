e0f1de9d77101fe17cd0f474d2e78ebf584d8388
nicholas-karimi <kariminic@gmail.com>
2022-06-03 12:19:24 +0300

    create landing page

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from leads.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leads/', include('leads.urls', namespace='leads'))
+    path('', index, name='index'),
+    path('leads/', include('leads.urls', namespace='leads')),
 
 ]
--- crm/leads/views.py
+++ crm/leads/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
+def index(request):
+    return render(request, 'landing.html')
+
 def leads_list(request):
     leads = Lead.objects.all()
 
